63e71a526bf16d9e794b4ed254a0644f4ee22195
delm83 <delm83@googlemail.com>
2024-02-10 14:28:32 +0000

    added error checks to api/solve

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,18 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let puzzle = req.body.puzzle;
+      let coordinate = req.body.coordinate;
+      let value = req.body.value;
 
     });
     
   app.route('/api/solve')
     .post((req, res) => {
-
+let puzzle = req.body.puzzle;
+if(!puzzle){return res.json({error: 'Required field missing'});}
+if(/[^0-9.]/.test(puzzle)){return res.json({error: 'Invalid characters in puzzle' })};
+if(puzzle.length!=81){return  res.json({error: 'Expected puzzle to be 81 characters long' })};
+return res.json({solution: 'solution'});
     });
 };
