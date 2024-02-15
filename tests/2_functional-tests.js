4f8844783887cb5b2f0cc0968f7ec143ee3e01b0
delm83 <delm83@googlemail.com>
2024-02-15 13:47:07 +0000

    completed api/solve functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,6 +39,52 @@ suite('Functional Tests', () => {
       done();
     });
   });
+  test('Test POST /api/solve with invalid characters', function(done) {
+    chai.request(server)
+    .keepOpen()
+    .post('/api/solve')
+    .send({
+      puzzle: '..839.7.575.....964..1....!..16.29846.9.312.7..754.....62..5.78.8.*.3.2...492...1'
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.isObject(res.body, 'response should be an object');
+      assert.property(res.body, 'error', 'returned object should contain error');
+      assert.equal(res.body.error, 'Invalid characters in puzzle');
+      done();
+    });
+  });
+  test('Test POST /api/solve with incorrect length', function(done) {
+    chai.request(server)
+    .keepOpen()
+    .post('/api/solve')
+    .send({
+      puzzle: '..839.7.575.....964..1......16.29846.9.312.7..754.....62..5.78.8..3.2...492...1'
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.isObject(res.body, 'response should be an object');
+      assert.property(res.body, 'error', 'returned object should contain error');
+      assert.equal(res.body.error, 'Expected puzzle to be 81 characters long');
+      done();
+    });
+  });
+
+  test('Test POST /api/solve a puzzle that cannot be solved', function(done) {
+    chai.request(server)
+    .keepOpen()
+    .post('/api/solve')
+    .send({
+      puzzle: '.7.89.....5....3.4.2..44.1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6'
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.isObject(res.body, 'response should be an object');
+      assert.property(res.body, 'error', 'returned object should contain error');
+      assert.equal(res.body.error, 'Puzzle cannot be solved');
+      done();
+    });
+  });
 
 });
 
